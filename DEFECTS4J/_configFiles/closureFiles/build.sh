0e0e44756b1dd3ac02631655e5754b7bc2433688
Maes95 <michel.maes95@gmail.com>
2020-11-06 13:48:24 +0100

    Add E2E Dataset experiment: WebApp-1

--- DEFECTS4J/_configFiles/closureFiles/build.sh
+++ DEFECTS4J/_configFiles/closureFiles/build.sh
@@ -1,2 +1,4 @@
-cp _configFiles/closureFiles/build.xml ClosureBase/build.xml
-cp _configFiles/closureFiles/GetMetrics.java ClosureBase/test/com/google/GetMetrics.java
+PROJECT_FOLDER=/tmp/ClosureBase
+BASE_FOLDER=/home/ubuntu/defects4j
+cp $BASE_FOLDER/_configFiles/closureFiles/build.xml $PROJECT_FOLDER/build.xml
+cp $BASE_FOLDER/_configFiles/closureFiles/GetMetrics.java $PROJECT_FOLDER/test/com/google/GetMetrics.java
---
+++ E2E-DATASET/extractMetrics.py
@@ -0,0 +1,107 @@
+import json
+import pandas as pd
+import re
+from os import listdir
+from os.path import isfile, isdir, join
+
+def cleanKey(key):
+    return key.replace('.', '_').replace("-", "_")
+
+def extractTestCaseData(data):
+
+    # All executions have only 1 Test Suite with 1 Test Case
+    ts = data['tJobExec']['testSuites'][0]
+
+    row = {
+        'app': data['tJobExec']['tJob']['sut']['name'],
+        'name': ts['testCases'][0]['name'],  # ts['name'],
+        'time': data['tJobExec']['duration'],
+        'testCases': [],
+        'maxMem': 0,
+        'maxCpu': 0
+    }
+
+    for tc in ts['testCases']:
+        #print("    - "+tc['name'])
+
+        tc_dict = {
+            "name": tc['name'],
+            "time": tc['time'],
+            "maxMem": [],
+            "maxCpu": []
+        }
+
+        memMetrics = []
+        cpuMetrics = []
+
+        if 'metrics' in tc:
+
+            for item in tc['metrics']:
+
+                # MEMORY USAGE
+
+                if "_dockbeat-memory.maxUsage" in item['name']:
+                    match = re.search("(.*)-et_dockbeat-memory.maxUsage", item['name'])
+                    component = match.group(1)
+                    #print("       Component: %s" % component)
+                    for metric in item['traces'][cleanKey(item['name'])]:
+                        memInMb = (metric['value'] / 1024) / 1024
+                        #print("       -> Memory (MBytes): %d" % memInMb)
+                        memMetrics.append((metric['value'] / 1024) / 1024)
+
+                # CPU USAGE
+
+                if "_dockbeat-cpu.totalUsage" in item['name']:
+                    match = re.search("(.*)-et_dockbeat-cpu.totalUsage", item['name'])
+                    component = match.group(1)
+                    #print("       Component: %s" % component)
+                    for metric in item['traces'][cleanKey(item['name'])]:
+                        cpuUsage= metric['value']
+                        #print("       -> CPU (%%): %f" % cpuUsage)
+                        cpuMetrics.append(cpuUsage)
+
+        tc_dict['maxMem'] = max(memMetrics, default=0)
+        tc_dict['maxCpu'] = max(cpuMetrics, default=0)
+        row['testCases'].append(tc_dict)
+
+    row['maxMem'] = max(map(lambda x: x["maxMem"], row['testCases']))
+    row['maxCpu'] = max(map(lambda x: x["maxCpu"], row['testCases']))
+    del row['testCases']
+
+    return row
+
+
+if __name__ == "__main__":
+
+    MAIN_PATH = "/home/ubuntu/E2EDataset"
+
+    apps = [f for f in listdir(MAIN_PATH) if isdir(join(MAIN_PATH, f))]
+
+    # ITERATE APPS
+
+    for app in apps:
+        app_path = join(MAIN_PATH, app)
+        test_folders = [f for f in listdir(app_path) if isdir(join(app_path, f))]
+
+        # ITERATE TEST RESULTS FOLDERS
+
+        for test_folder in test_folders:
+            test_results_path = join(app_path, test_folder)
+            results = [f for f in listdir(test_results_path) if isfile(join(test_results_path, f)) and f.endswith(".json")]
+
+            # ITERATE TEST RESULTS ATTEMPS
+
+            attemps = []
+
+            for result in results:
+                
+                with open(join(test_results_path, result)) as json_file:
+                    data = json.load(json_file)
+                    attemps.append(extractTestCaseData(data))
+
+            if len(attemps) > 0:
+                df = pd.DataFrame.from_dict(attemps)
+                df.to_csv(join(test_results_path,'results.csv'))
+                means = df.groupby(['app','name']).mean()
+                means.to_csv(join(test_results_path,'mean_of_results.csv'))
+                print(df)
