29739044c2ef715f437ec55427bf19c7cae35986
Maes95 <michel.maes95@gmail.com>
2020-11-16 18:42:44 +0100

    Update Closure config

--- DEFECTS4J/_configFiles/closureFiles/build.sh
+++ DEFECTS4J/_configFiles/closureFiles/build.sh
@@ -1,4 +1,3 @@
 PROJECT_FOLDER=/tmp/ClosureBase
 BASE_FOLDER=/home/ubuntu/defects4j
-cd /tmp/ChartBase/ && git checkout trunk
 cp $BASE_FOLDER/_configFiles/closureFiles/build.xml $PROJECT_FOLDER/build.xml
